cc1a4176dc08253e899877e81d348eeb1cecb84a
technoid2023 <136116887+technoid2023@users.noreply.github.com>
2024-01-09 10:36:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar(){
         <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
   <div className="container-fluid">
     <Link className="navbar-brand" to="#">Navbar</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
